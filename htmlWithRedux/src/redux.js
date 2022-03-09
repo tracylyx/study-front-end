e78eaca5ff8aafcdb22fbbdc000fb37de142da03
yxli <yxli@trip.com>
2022-03-09 23:23:01 +0800

    feat: redux code

--- htmlWithRedux/src/redux.js
+++ htmlWithRedux/src/redux.js
@@ -1,8 +1,12 @@
-export default function createStore(reducer, initialState)
+const createStore = function (reducer, initialState)
 {
+    // 内存变量
     let currentReducer = reducer;
     let currentState = initialState;
     let isDispatching = false;
+    let currentListener = [];
+    let nextListener = currentListener;
+    let isSubscribe = false;
 
     const getState = () => {
         return currentState;
@@ -11,17 +15,45 @@ export default function createStore(reducer, initialState)
     const dispatch = (action) => {
         try {
             isDispatching = true
-            currentState = currentReducer(action);
+            currentState = currentReducer(currentState, action);
         } finally {
             isDispatching = false;
         }
 
+        const listener = (currentListener = nextListener);
+        for(let i = 0; i < listener.length; i++) {
+            listener[i]();
+        }
         return action;
     };
 
-    const subscribe = () => {
+    const subscribe = (listener) => {
+        isSubscribe = true;
+        nextListener.push(listener);
 
+        return function () {
+            isSubscribe = false;
+        }
     };
+
+    const ActionTypes = {
+        INIT:
+            '@@redux/INIT' +
+            Math.random()
+                .toString(36)
+                .substring(7)
+                .split('')
+                .join('.'),
+        REPLACE:
+            '@@redux/REPLACE' +
+            Math.random()
+                .toString(36)
+                .substring(7)
+                .split('')
+                .join('.')
+    };
+
+    dispatch({ type: ActionTypes.INIT });
 
     return {
         dispatch,
@@ -29,3 +61,10 @@ export default function createStore(reducer, initialState)
         subscribe
     }
 }
+
+export {
+    createStore
+}
+
+// Q：createStore， 传入 initialState 和 reducer 中设置初始值的区别
+// A：传入 initialState， 这样在调用createState就会给内存变量state附上值
